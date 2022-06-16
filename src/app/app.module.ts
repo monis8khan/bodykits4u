418a96d0ec9cf1f4963e143f70f803608523bbd7
Monis Hassan <mhassan@trafix.com>
2022-06-16 13:56:54 +0500

    components created nav bar for desktop created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,26 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { HeroComponent } from './components/hero/hero.component';
+import { ServicesComponent } from './components/services/services.component';
+import { ProductsComponent } from './components/products/products.component';
+import { TestimonialsComponent } from './components/testimonials/testimonials.component';
+import { QueryFormComponent } from './components/query-form/query-form.component';
+import { ProjectGalleryComponent } from './components/project-gallery/project-gallery.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    HeroComponent,
+    ServicesComponent,
+    ProductsComponent,
+    TestimonialsComponent,
+    QueryFormComponent,
+    ProjectGalleryComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,22 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+    },
+    fontFamily: {
+      display: ['Gilroy', 'sans-serif'],
+      body: ['Graphik', 'sans-serif'],
+    },
+    borderWidth: {
+      default: '1px',
+      '0': '0',
+      '2': '2px',
+      '4': '4px',
+    },},
   },
   plugins: [],
 }
