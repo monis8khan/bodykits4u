2b9cc3161fa71ac14c717de56b923308422687d2
Monis Hassan <mhassan@trafix.com>
2022-06-20 15:52:11 +0500

    added the theme colors and set out the nav bar

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,12 @@ module.exports = {
       lg: '1024px',
       xl: '1280px',
     },
+    colors: {
+      primary: '#99e1d9',
+      secondary: '#70abaf',
+      tertiary: '#32292f',
+      base: '#f0f7f4'
+    },
     fontFamily: {
       display: ['Gilroy', 'sans-serif'],
       body: ['Graphik', 'sans-serif'],
