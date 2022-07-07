e35376c785d7876e4a7b5a578693eca71deb05a7
monis8khan@gmail.com <monis8khan@gmail.com>
2022-07-07 16:30:03 +0500

    Mobile menu added

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -7,9 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavBarComponent implements OnInit {
 
+  btn:any;
+  nav:any;
   constructor() { }
 
   ngOnInit(): void {
+    this.btn = document.getElementById('menu-btn')
+    this.nav = document.getElementById('menu')
+  }
+
+  toggleMobileMenu() {
+    this.btn.classList.toggle("open")
+    this.nav.classList.toggle('flex')
+    this.nav.classList.toggle('hidden')
+        
   }
 
 }
